e1283a19d1ef2a8eb7aafa5d51f9558403aff2c9
Julie Jacobson <juliejacobson@Julies-MacBook-Pro-2.local>
2020-11-01 14:24:11 -0800

    transfer from bitbucket to github

---
+++ app/src/main/java/edu/sjsu/android/accelerometer/MainActivity.java
@@ -0,0 +1,44 @@
+package edu.sjsu.android.accelerometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "edu.sjsu.android.accelerometer:MainActivity";
+    private PowerManager.WakeLock mWakeLock;
+    private SimulationView mSimulationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        PowerManager mPowerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        mWakeLock = mPowerManager.newWakeLock(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, TAG);
+        mSimulationView = new SimulationView(this);
+        // Set to the simulation view instead of layout file.
+        setContentView(mSimulationView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // acquire wakelock
+        mWakeLock.acquire();
+        // start simulation to register the listener
+        mSimulationView.startSimulation();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Release wakelock
+        mWakeLock.release();
+        // stop simulation to unregister the listener
+        mSimulationView.stopSimulation();
+    }
+
+}
---
+++ app/src/main/java/edu/sjsu/android/accelerometer/SimulationView.java
@@ -0,0 +1,126 @@
+package edu.sjsu.android.accelerometer;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.Display;
+import android.view.Surface;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SimulationView extends View implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private Display mDisplay;
+
+
+    float mSensorX;
+    float mSensorY;
+    float mSensorZ;
+    long mSensorTimeStamp;
+
+    private Bitmap mField; // field pic
+    private Bitmap mBasket; // basket pic
+    private Bitmap mBitmap;
+    private static final int BALL_SIZE = 64;
+    private static final int BASKET_SIZE = 90;
+
+    private Particle mBall = new Particle();
+
+    private float mXOrigin;
+    private float mYOrigin;
+    private float mHorizontalBound;
+    private float mVerticalBound;
+
+
+    public SimulationView(Context context) {
+        // initialize images
+        super(context);
+        Bitmap ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+        mBitmap = Bitmap.createScaledBitmap(ball, BALL_SIZE, BALL_SIZE, true);
+        Bitmap basket = BitmapFactory.decodeResource(getResources(), R.drawable.basket);
+        mBasket = Bitmap.createScaledBitmap(basket, BASKET_SIZE, BASKET_SIZE, true);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inPreferredConfig = Bitmap.Config.RGB_565;
+        mField = BitmapFactory.decodeResource(getResources(), R.drawable.field, opts);
+        WindowManager mWindowManager = (WindowManager)
+                context.getSystemService(Context.WINDOW_SERVICE);
+        mDisplay = mWindowManager.getDefaultDisplay();
+
+        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int width, int height)
+    {
+        mXOrigin = w * 0.5f;
+        mYOrigin = h * 0.5f;
+
+        mHorizontalBound = (w - BALL_SIZE) * 0.5f;
+        mVerticalBound = (h - BALL_SIZE) * 0.5f;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+            return;
+        if (mDisplay.getRotation() == Surface.ROTATION_0) {
+            mSensorX = sensorEvent.values[0];
+            mSensorY = sensorEvent.values[1];
+        }
+
+        else if (mDisplay.getRotation() == Surface.ROTATION_90) {
+            mSensorX = -sensorEvent.values[1];
+            mSensorY = sensorEvent.values[0];
+        }
+        else if (mDisplay.getRotation() == Surface.ROTATION_180) {
+            mSensorX = -sensorEvent.values[1];
+            mSensorY = sensorEvent.values[0];
+        }
+        else if (mDisplay.getRotation() == Surface.ROTATION_270) {
+            mSensorX = -sensorEvent.values[1];
+            mSensorY = sensorEvent.values[0];
+        }
+
+
+        mSensorZ = sensorEvent.values[2];
+        mSensorTimeStamp = sensorEvent.timestamp;
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) { }
+
+
+    public void startSimulation() {
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void stopSimulation() {
+        sensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(mField, 0, 0, null);
+        canvas.drawBitmap(mBasket, mXOrigin - BASKET_SIZE / 2, mYOrigin - BASKET_SIZE / 2, null);
+        mBall.updatePosition(mSensorX, mSensorY, mSensorZ, mSensorTimeStamp);
+        mBall.resolveCollisionWithBounds(mHorizontalBound, mVerticalBound);
+        canvas.drawBitmap(mBitmap,
+                (mXOrigin - BALL_SIZE / 2) + mBall.mPosX,
+                (mYOrigin - BALL_SIZE / 2) - mBall.mPosY, null);
+        invalidate();
+    }
+
+
+
+}
